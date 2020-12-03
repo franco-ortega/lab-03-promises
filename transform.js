4cae553c2a307d2c7190a017cd6a76911f00932e
Franco Ortega <francopdx@gmail.com>
2020-12-02 18:09:10 -0800

    add test for transform function and update transform function

--- transform.js
+++ transform.js
@@ -3,37 +3,24 @@ const fsPromises = require('fs').promises;
 function transform(src) {
     return fsPromises.readFile(src, 'utf-8')
     .then(file => {
-        for(let i = 0; i < file.length; i++) {
-            const letter = file[i];
-
-            if(letter === letter.toUpperCase()) {
-                //letters.push(letter);
-                file = file.replace(letter, ' ')
-            }
-        }
-        return file;
+        console.log(file);
+        return file.split('').filter(letter => {
+            return letter === letter.toLowerCase()
+        })
     })
     .then(file => {
-        const upperCaseLetters = file.toUpperCase();
-        return upperCaseLetters;
+        return file.map(letter => {
+            return letter.toUpperCase();
+        })
     })
-    .then(upperCaseLetters => {
-        console.log(upperCaseLetters);
-        return upperCaseLetters.split('').reverse().join('');
-    })
-    .then(upperCaseLetters => {
-        console.log(upperCaseLetters);
+    .then(file => {
+        return file.reverse().join('');
     })
     .catch(err => {
         console.log(err);
     })
-    
-    
-    
 
 }
-
-transform('./capital.md');
 
 module.exports = {
     transform
