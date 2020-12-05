31c4c91890bcbe7b37977624b17d5c737373996f
Franco Ortega <francopdx@gmail.com>
2020-12-04 17:04:43 -0800

    add initial framework with test data for rickAndMortyApi() function

---
+++ sandbox/rickAndMortyApi.js
@@ -0,0 +1,12 @@
+function rickandMortyApi(characterId) {
+    const characterInfo = {
+        name: 'name',
+        status: 'status',
+        species: 'species'
+    }
+    return characterInfo;
+}
+
+module.exports = {
+    rickandMortyApi
+}
