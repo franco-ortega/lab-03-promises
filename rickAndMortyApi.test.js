36b40c1c467ad5174eb450e698ea4f4d1b8cca1c
Franco Ortega <francopdx@gmail.com>
2020-12-05 14:16:30 -0800

    add second expectation to rickAndMortyApi() test

--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -2,15 +2,25 @@ const { rickandMortyApi } = require('./rickAndMortyApi');
 
 describe('rickAndMortyApi', () => {
     it('takes a character ID and returns the name, status, and species values of the character', async() => {
-        const characterId = 2;
-        const expected = {
+
+        const characterId1 = 1;
+        const expected1 = {
+            name: "Rick Sanchez",
+            status: "Alive",
+            species: "Human"
+        }
+
+        const characterId2 = 2;
+        const expected2 = {
             name: "Morty Smith",
             status: "Alive",
             species: "Human"
         }
 
-        const result = await rickandMortyApi(characterId);
+        const result1 = await rickandMortyApi(characterId1);
+        const result2 = await rickandMortyApi(characterId2);
 
-        expect(result).toEqual(expected)
+        expect(result1).toEqual(expected1)
+        expect(result2).toEqual(expected2)
     })
 })
