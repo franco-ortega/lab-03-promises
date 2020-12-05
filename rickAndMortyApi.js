38b1d4b41c17f0373f52b8df75d3e4c711b57e12
Franco Ortega <francopdx@gmail.com>
2020-12-05 14:57:00 -0800

    refactor rickAndMortyApi() function with async/await

--- rickAndMortyApi.js
+++ rickAndMortyApi.js
@@ -1,20 +1,28 @@
 const fetch = require('node-fetch');
-
+console.log('start of file')
 const URL = 'https://rickandmortyapi.com/api/character/';
 
-function rickandMortyApi(characterId) {
-    
-    return fetch(`${URL}${characterId}`)
-    .then(response => response.json())
-    .then(results => {        
-        return {
-            name: results.name,
-            status: results.status,
-            species: results.species
-        }
-    })
-}
+async function rickandMortyApi(characterId) {
+    const response = await fetch(`${URL}${characterId}`)
+    const results = await response.json();
+    const info = {
+        name: results.name,
+        status: results.status,
+        species: results.species
+    }
+    return info;
+    }
 
 module.exports = {
     rickandMortyApi
 }
+
+// return fetch(`${URL}${characterId}`)
+    // .then(response => response.json())
+    // .then(results => {        
+        // return {
+        //     name: results.name,
+        //     status: results.status,
+        //     species: results.species
+    //     }
+    // })
