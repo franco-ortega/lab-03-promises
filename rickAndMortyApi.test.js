5b06561d8848ce096c9aa7f0087845aa3717e968
Franco Ortega <francopdx@gmail.com>
2020-12-05 14:12:19 -0800

    refactor rickAndMortyApi() test

--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -1,15 +1,15 @@
 const { rickandMortyApi } = require('./rickAndMortyApi');
 
 describe('rickAndMortyApi', () => {
-    const expected = {
-        name: "Morty Smith",
-        status: "Alive",
-        species: "Human"
-    }
     it('takes a character ID and returns the name, status, and species values of the character', async() => {
-        // const characterId = 2;
-        // const characterInfo = rickandMortyApi(characterId);
-        const result = await rickandMortyApi(2);
+        const characterId = 2;
+        const expected = {
+            name: "Morty Smith",
+            status: "Alive",
+            species: "Human"
+        }
+
+        const result = await rickandMortyApi(characterId);
 
         expect(result).toEqual(expected)
     })
