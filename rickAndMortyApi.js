e501f74f1348b3a7e3a1a7de0bd27a22b94dc95e
Franco Ortega <francopdx@gmail.com>
2020-12-05 15:09:20 -0800

    rename rickAndMortyApi() function to getCharacter()

--- rickAndMortyApi.js
+++ rickAndMortyApi.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 console.log('start of file')
 const URL = 'https://rickandmortyapi.com/api/character/';
 
-const rickandMortyApi = async(characterId) => {
+const getCharacter = async(characterId) => {
     const response = await fetch(`${URL}${characterId}`)
     const results = await response.json();
     const info = {
@@ -14,7 +14,7 @@ const rickandMortyApi = async(characterId) => {
     }
 
 module.exports = {
-    rickandMortyApi
+    getCharacter
 }
 
 // return fetch(`${URL}${characterId}`)
--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -1,15 +1,15 @@
-const { rickandMortyApi } = require('./rickAndMortyApi');
+const { getCharacter } = require('./rickAndMortyApi');
 
 describe('rickAndMortyApi', () => {
     it('takes a character ID and returns the name, status, and species values of the character', async() => {
-
+    
         const characterId1 = 1;
         const expected1 = {
             name: "Rick Sanchez",
             status: "Alive",
             species: "Human"
         }
-
+        
         const characterId2 = 2;
         const expected2 = {
             name: "Morty Smith",
@@ -17,8 +17,8 @@ describe('rickAndMortyApi', () => {
             species: "Human"
         }
 
-        const result1 = await rickandMortyApi(characterId1);
-        const result2 = await rickandMortyApi(characterId2);
+        const result1 = await getCharacter(characterId1);
+        const result2 = await getCharacter(characterId2);
 
         expect(result1).toEqual(expected1)
         expect(result2).toEqual(expected2)
