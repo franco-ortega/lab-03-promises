a71285e60750d876abbc8bfba8648f63921535e5
Franco Ortega <francopdx@gmail.com>
2020-12-05 17:14:41 -0800

    add mocks to tests in rickAndMortyApi.test.js

--- rickAndMortyApi.test.js
+++ rickAndMortyApi.test.js
@@ -1,53 +1,44 @@
+const { default: fetch } = require('node-fetch');
 const { getCharacter, getManyCharacters } = require('./rickAndMortyApi');
+
+jest.mock('node-fetch');
 
 describe('rickAndMortyApi', () => {
     it('takes a character ID and returns the name, status, and species values of the character', async() => {
+        fetch.mockResolvedValue({
+            json: () => Promise.resolve({
+                name: expect.any(String),
+                status: expect.any(String),
+                species: expect.any(String)
+            })
+        })
     
-        const characterId1 = 1;
-        const expected1 = {
-            name: "Rick Sanchez",
-            status: "Alive",
-            species: "Human"
-        }
-        
+        const characterId1 = 1;    
         const characterId2 = 2;
-        const expected2 = {
-            name: "Morty Smith",
-            status: "Alive",
-            species: "Human"
-        }
 
         const result1 = await getCharacter(characterId1);
         const result2 = await getCharacter(characterId2);
 
-        expect(result1).toEqual(expected1)
-        expect(result2).toEqual(expected2)
+        expect(result1).toEqual({ name: "Rick Sanchez", status: "Alive", species: "Human" })
+        expect(result2).toEqual({ name: "Morty Smith", status: "Alive", species: "Human" })
     })
 
     it('takes an array of character IDs and returns a promise with an array of those characters', async() => {
-    
+        fetch.mockResolvedValue({
+            json: () => Promise.resolve({
+                name: expect.any(String),
+                status: expect.any(String),
+                species: expect.any(String)
+            })
+        })
+
         const characterIds = [1, 2]
-        const expected = [
-            {
-                name: "Rick Sanchez",
-                status: "Alive",
-                species: "Human"
-            },
-            {
-                name: "Morty Smith",
-                status: "Alive",
-                species: "Human"
-            }
-        ];
 
         const result = await getManyCharacters(characterIds);
-        
 
-        expect(result).toEqual(expected)
-        
-    })
-
-
-
-
-})
+        expect(result).toEqual([
+            { name: "Rick Sanchez", status: "Alive", species: "Human" },
+            { name: "Morty Smith", status: "Alive", species: "Human" }
+        ]);
+    });
+});
